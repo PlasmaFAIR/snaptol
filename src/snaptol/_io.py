e567a33bdc9d169c30895d6990d32fa54a6d2041
Ava Dean <ava.dean@york.ac.uk>
2025-11-19 18:22:52 +0000

    Add doc strings to relevant functions.

--- src/snaptol/_io.py
+++ src/snaptol/_io.py
@@ -6,10 +6,33 @@ from numpy import ndarray
 
 
 def snapshot_filename(test_name: str, test_file: Path) -> Path:
+    """
+    Generates a snapshot filename based on the test name and file path. Returns a Path object
+    with a '.json' extension.
+
+    Parameters
+    ----------
+    test_name : str
+        The name of the test for which the snapshot is being created.
+    test_file : Path
+        The path to the test file containing the test.
+    """
+
     return test_file.parent / "__snapshots__" / f"{test_file.stem}.{test_name}.json"
 
 
 def write_snapshot(snapshot_file: Path, value: Any):
+    """
+    Writes a snapshot to its file in JSON format.
+
+    Parameters
+    ----------
+    snapshot_file
+        The path where the snapshot file will be written.
+    value
+        The value to be serialised and written to the snapshot file.
+    """
+
     jsoned = dumps(value, indent=2, default=_json_fallback)
 
     snapshot_file.parent.mkdir(parents=True, exist_ok=True)
@@ -17,10 +40,30 @@ def write_snapshot(snapshot_file: Path, value: Any):
 
 
 def read_snapshot(snapshot_file: Path) -> Any:
+    """
+    Reads and deserialises a snapshot from a JSON file. Returns the deserialised content.
+
+    Parameters
+    ----------
+    snapshot_file : Path
+        The path to the snapshot file to be read.
+    """
+
     return loads(snapshot_file.read_text(encoding="utf-8"))
 
 
 def _json_fallback(value: Any) -> Any:
+    """
+    A fallback function for JSON serialisation that handles special data types.
+    Converts numpy arrays to lists and other non-serialisable objects to their string representation.
+    Returns the serialised value.
+
+    Parameters
+    ----------
+    value : Any
+        The value to be serialised to JSON format.
+    """
+
     try:
         if isinstance(value, ndarray):
             return value.tolist()
